61f5c7dfe7375269debb5cbbe3957a3cb187bcbb
satishwaghela <satishmwaghela@gmail.com>
2020-07-21 15:40:18 +0530

    Example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,142 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import useForm from 'form-react-hook';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import { MemoFTextField } from './ReusableFields/FTextField';
+import { MemoFCheckbox } from './ReusableFields/FCheckbox';
+import { MemoFAutoComplete } from './ReusableFields/FAutoComplete';
+import { MemoFCheckboxGroup } from './ReusableFields/FCheckboxGroup';
+import { MemoFRadioGroup } from './ReusableFields/FRadioGroup';
+import ArrayFieldExample from './ArrayFieldExample';
+import { requiredValidation } from './FormValidations';
 
-function App() {
+const App = () => {
+  const submitBtnRef = React.createRef();
+
+  const form = useForm({
+    formData: {}
+  });
+
+  const { formState } = form;
+  useEffect(() => {
+    form.getFormValidity((validity) => {
+      if (validity.valid) {
+        submitBtnRef.current.style.opacity = 1;
+      } else {
+        submitBtnRef.current.style.opacity = 0.5;
+      }
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formState]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form>
+      <Container>
+        <MemoFTextField
+          form={form}
+          fieldKeyPath='profile.firstname'
+          validation={(value, formState, callback) => {
+            const errorMsg = requiredValidation(value);
+            callback(errorMsg);
+          }}
+          TextFieldProps={{
+            label: 'First Name'
+          }}
+        />
+        <MemoFTextField
+          form={form}
+          fieldKeyPath='profile.lastname'
+          validation={(value, formState, callback) => {
+            const errorMsg = requiredValidation(value);
+            callback(errorMsg);
+          }}
+          TextFieldProps={{
+            label: 'Last Name'
+          }}
+        />
+        <MemoFRadioGroup
+          form={form}
+          fieldKeyPath='gender'
+          radioOptions={[{
+            label: 'Female',
+            value: 'female'
+          }, {
+            label: 'Male',
+            value: 'male'
+          }, {
+            label: 'Other',
+            value: 'other'
+          }]}
+          validation={(value, formState, callback) => {
+            const errorMsg = requiredValidation(value);
+            callback(errorMsg);
+          }}
+        />
+        <MemoFAutoComplete
+          form={form}
+          fieldKeyPath='dummyname'
+          valueKey='name'
+          AutocompleteProps={{
+            multiple: true,
+            options: [{ name: 'Bob' }, { name: 'Alice' }],
+            getOptionLabel: (option) => option.name
+          }}
+          TextFieldProps={{
+            label: 'Combo box'
+          }}
+        />
+        <MemoFCheckboxGroup
+          form={form}
+          fieldKeyPath='assignResp'
+          checkboxOptions={[{
+            label: 'Gilad Gray',
+            value: 'Gilad Gray'
+          }, {
+            label: 'Jason Killian',
+            value: 'Jason Killian'
+          }, {
+            label: 'Antoine Llorca',
+            value: 'Antoine Llorca'
+          }]}
+          validation={(value, formState, callback) => {
+            const errorMsg = requiredValidation(value);
+            callback(errorMsg);
+          }}
+        />
+        <ArrayFieldExample form={form} />
+        <MemoFCheckbox
+          form={form}
+          fieldKeyPath='ack'
+          validation={(value, formState, callback) => {
+            const errorMsg = value ? '' : 'Please accept terms & conditions';
+            callback(errorMsg);
+          }}
+          FormControlLabelProps={{
+            label: 'Terms & Conditions'
+          }}
+        />
+        <Grid item xs={12}>
+          <Button
+            ref={submitBtnRef}
+            variant='contained' color='primary'
+            onClick={() => {
+              form.getFormValidity((validity) => {
+                if (validity.valid) {
+                  console.log(form.formState.formData);
+                } else {
+                  console.log(validity);
+                  form.validateForm();
+                }
+              });
+            }}
+          >
+            Submit
+          </Button>
+        </Grid>
+      </Container>
+    </form>
   );
 }
 
-export default App;
+export default App
